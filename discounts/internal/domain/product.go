1fcdf9ca22db34a9fee3b1a1487784c545299c94
Gabriel Felix <crowleyfelix@gmail.com>
2021-05-02 16:10:23 -0300

    Adiciona lógica de calculo de descontos

---
+++ discounts/internal/domain/product.go
@@ -0,0 +1,49 @@
+package domain
+
+type Product struct {
+	ID           string
+	PriceInCents int64
+	Title        string
+	Description  string
+	calcs        []DiscountCalculator
+}
+
+func (p *Product) AddCalculators(calcs ...DiscountCalculator) {
+	for _, calc := range calcs {
+		p.calcs = append(p.calcs, calc)
+	}
+}
+
+func (p *Product) Discount() *Discount {
+	c := make(chan *Discount, len(p.calcs))
+
+	for _, calc := range p.calcs {
+
+		go func(calc DiscountCalculator, out chan *Discount) {
+			out <- calc.Calculate(p.PriceInCents)
+		}(calc, c)
+	}
+
+	disc := new(Discount)
+
+	for i := 0; i < len(p.calcs); i++ {
+		d := <-c
+
+		if d == nil {
+			continue
+		}
+
+		disc.Percentage = disc.Percentage + d.Percentage
+		disc.ValueInCents = disc.ValueInCents + d.ValueInCents
+
+		if disc.Percentage >= maxDiscountPerc {
+			return new(maxDiscountRule).Calculate(p.PriceInCents)
+		}
+	}
+
+	if disc.Percentage == 0 {
+		return nil
+	}
+
+	return disc
+}
---
+++ discounts/internal/domain/product_test.go
@@ -0,0 +1,76 @@
+package domain
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type calculatorMock struct{ should bool }
+
+func (m *calculatorMock) Calculate(price int64) *Discount {
+	if !m.should {
+		return nil
+	}
+
+	perc := 0.05
+	return &Discount{
+		Percentage:   0.05,
+		ValueInCents: int64(float64(price) * perc),
+	}
+}
+
+var _ = Describe("Product", func() {
+	var (
+		product Product
+	)
+
+	BeforeEach(func() { product = Product{PriceInCents: 100} })
+
+	Describe("applying a discount", func() {
+		var (
+			discount *Discount
+		)
+
+		JustBeforeEach(func() { discount = product.Discount() })
+
+		Context("when no rule is appliable", func() {
+			BeforeEach(func() { product.AddCalculators(&calculatorMock{false}) })
+
+			It("should not return a discount", func() {
+				Expect(discount).To(BeNil())
+			})
+		})
+
+		Context("when one rule is appliable", func() {
+			BeforeEach(func() { product.AddCalculators(&calculatorMock{false}, &calculatorMock{true}) })
+
+			It("should not return a discount", func() {
+				Expect(discount).ToNot(BeNil())
+				Expect(discount.Percentage).To(BeEquivalentTo(0.05))
+				Expect(discount.ValueInCents).To(BeEquivalentTo(5))
+			})
+		})
+
+		Context("when two rules is appliable", func() {
+			BeforeEach(func() { product.AddCalculators(&calculatorMock{true}, &calculatorMock{true}) })
+
+			It("should not return a discount", func() {
+				Expect(discount).ToNot(BeNil())
+				Expect(discount.Percentage).To(BeEquivalentTo(0.1))
+				Expect(discount.ValueInCents).To(BeEquivalentTo(10))
+			})
+		})
+
+		Context("when max discount exceeded", func() {
+			BeforeEach(func() {
+				product.AddCalculators(&calculatorMock{true}, &calculatorMock{true}, &calculatorMock{true}, &calculatorMock{true})
+			})
+
+			It("should return a max discount", func() {
+				Expect(discount).ToNot(BeNil())
+				Expect(discount.Percentage).To(BeEquivalentTo(0.1))
+				Expect(discount.ValueInCents).To(BeEquivalentTo(10))
+			})
+		})
+	})
+})
